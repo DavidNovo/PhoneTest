a919c0cfc1eb71142dbd67083c407a4419ba702c
David Novogrodsky_WRK <davidnovogrodsky_wrk@david-ns-macbook-pro.local>
2017-05-07 14:27:30 -0500

    initial files for this project

---
+++ app/src/main/java/novogrodsky/phonetest/MainActivity.java
@@ -0,0 +1,21 @@
+package novogrodsky.phonetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button **/
+    public void SendMessage(View view) {
+        // do something in response to button
+    }
+
+
+}
